8a721c75d3d6253753cbebe7e0c5e0d4400c35dc
dbyl <bylicki.damian@gmail.com>
2023-07-09 17:06:44 +0200

    quicksave

--- source/pepper_app/populate_database.py
+++ source/pepper_app/populate_database.py
@@ -10,13 +10,16 @@ from pepper_app.models import PepperArticles, Statistics
 
 class LoadItemDetailesToDatabase(BaseCommand):
 
+    def __init__(self, item: List[str]) -> None:
+        self.article = article
     
-    def load_to_db(self, item):
+    def load_to_db(self) -> None:
         bad = 0
         good = 0
         header = ['item_id', 'name', 'discount_price', 
-                'percentage_discount', 'regular_price', 'date_added', 'url']
-        item_df = pd.DataFrame(columns=header, data=item)
+                'percentage_discount', 'regular_price', 
+                'date_added', 'url']
+        item_df = pd.DataFrame(columns=header, data=self.item)
         start = datetime.datetime.now
         for _, row in item_df.iterrows():
             try:
@@ -31,24 +34,24 @@ class LoadItemDetailesToDatabase(BaseCommand):
                 )
                 good += 1
                 now = datetime.datetime.now
-                print(
-                    f"goods: {good}, loading time: {start-now}",
-                )
+                logger.info(f"goods: {good}, loading time: {start-now}")
             except Exception as e:
                 bad += 1
-                with open("data_load_logging.txt", "w") as bad_row:
+                with open("populating_db_errors.txt", "w") as bad_row:
                     bad_row.write(f"Error message: {e} \n")
 
 
 class LoadDataFromCsv(BaseCommand):
 
-    def add_arguments(self, parser):
-        parser.add_argument(
+    def __init__(self, parser: str) -> None:
+        self.parser = parser
+
+    def add_arguments(self) -> None:
+        self.parser.add_argument(
             "input", type=str, help="Choose directory path with input csv files"
         )
 
     def handle(self, *args, **options):
-
         path = options["input"]
         logging.info(f"Preparing data from {path}...")
         df = self.read_csv(path)
