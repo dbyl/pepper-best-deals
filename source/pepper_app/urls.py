5f5e205fbe2a68498e35363d33dba058a8159e44
dbyl <bylicki.damian@gmail.com>
2023-11-16 22:16:19 +0100

    quicksave

--- source/pepper_app/urls.py
+++ source/pepper_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 from pepper_app import views
-from pepper_app.views import (action, post_action, pre_action, scrap_view, scrap_status, scrap_result, check_task_status)
+from pepper_app.views import (action, post_action, pre_action, scrap_view, scrap_status, session_check, scrap_result, check_task_status)
 
 urlpatterns = [
     path('pre_action/', views.pre_action, name="pre_action"),
@@ -10,5 +10,6 @@ urlpatterns = [
     path('scrap/', views.scrap_view, name="scrap"),
     path('scrap_status/<task_id>/', views.scrap_status, name="scrap_status"),
     path('scrap_result/<task_id>/', views.scrap_result, name="scrap_result"),
+    path('session_check/', views.session_check, name="session_check"),
     path('check_task_status/<task_id>/', views.check_task_status, name='check_task_status'),
       ]
